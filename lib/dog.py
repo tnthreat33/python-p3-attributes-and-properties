8884f878d64b41147248e0d126b53093a5cbf7ca
Rachel Hamby <tnthreat33@gmail.com>
2023-10-27 19:58:16 -0400

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,8 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def set_name(self, name):
+        if(type(name) in (str) and (1<= name <= 25)):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
